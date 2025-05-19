5ad4d4767b4874efc9149fdbeae0cf06fc741fa2
Allison Thackston <allison@allisonthackston.com>
2025-05-19 23:30:08 +0000

    Workflow again

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+# Set bundle path to local directory (inside project)
 bundle config set --local path 'vendor/bundle'
 
 # Install the version of Bundler.
-if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock >/dev/null; then
-    echo "Installing bundler in gemfile"
+if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
 fi
 
 # If there's a Gemfile, then run `bundle install`
 # It's assumed that the Gemfile will install Jekyll too
-bundle config set --local path 'vendor/bundle'
 if [ -f Gemfile ]; then
     bundle install
 fi
